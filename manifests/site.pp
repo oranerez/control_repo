fca296bd7a3d414fc8e84920a3a0d027a395ee4e
oranerez <38244819+oranerez@users.noreply.github.com>
2018-06-13 12:02:01 +0300

    Update site.pp
    Add new node definition for test node minetest.puppet.vm

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,6 +7,11 @@ node 'master.puppet.vm' {
     content => "Welcome to ${fqdn}\n",
   }
 }
+
+node 'minetest.puppet.vm' {
+  include role::minecraft_server
+}
+
 node /^web/ {
   include role::app_server
 }
